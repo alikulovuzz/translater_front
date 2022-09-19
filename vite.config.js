d084fd66de8115fa7bca12de36e7be93a2f5e709
ALIKULOV AZAMAT <40868285+alikulovuzz@users.noreply.github.com>
2022-09-19 10:50:22 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [splitVendorChunkPlugin()]
+  plugins: [react()],
+  server: {
+    port: 8080,
+    strictPort: true,
+    hmr: {
+        port: 9000
+    }
+}
 })
