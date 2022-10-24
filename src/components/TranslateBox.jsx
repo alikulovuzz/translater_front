6b843d86b4f98386abf5c1fe1d3997e9ac050bdf
shohruzbekcoder <shohruzbekochilov007@gmail.com>
2022-10-24 16:00:11 +0500

    file type wav without audio

--- src/components/TranslateBox.jsx
+++ src/components/TranslateBox.jsx
@@ -179,7 +179,7 @@ export const TranslateBox = () => {
           </div>
         </div>
       </div>
-      <video src={mediaBlobUrl} controls autoPlay loop />
+      {/* <video src={mediaBlobUrl} controls autoPlay loop /> */}
       {/* <Animation /> */}
     </>
   );
